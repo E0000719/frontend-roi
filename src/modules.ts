2d5a57800a0618640c149747a3d5e75d89ec9e5a
David García <david.garcia@saaskun.com>
2025-11-05 18:49:58 -0600

    Modules

--- src/modules.ts
+++ src/modules.ts
@@ -7,12 +7,12 @@ import AgenticCustomerITSupport from "./components/icons/AgenticCustomerITSuppor
 import PhysicalAgentServiceRobotsIcon from "./components/icons/PhysicalAgentServiceRobotsIcon";
 
 const modules = [
-  { name: "ROI First", href: "", icon: PhysicalAgentServiceRobotsIcon, iconColor: "text-green-400", active: true },
+  { name: "ROI First", href: "http://ec2-44-223-62-175.compute-1.amazonaws.com:9091/", icon: PhysicalAgentServiceRobotsIcon, iconColor: "text-green-400", active: true },
   { name: "Agentic Customer & IT Support", href: "http://ec2-44-223-62-175.compute-1.amazonaws.com:8080/", icon: AgenticCustomerITSupport, iconColor: "text-warning-300" },
-  { name: "Legal & Compliance", href: "", icon: LegalCompliance, iconColor: "text-green-300" },
+  { name: "Legal & Compliance", href: "http://automated.legal.compliance.roi-roomie.com:9080/", icon: LegalCompliance, iconColor: "text-green-300" },
   { name: "Order to Cash", href: "http://ec2-44-223-62-175.compute-1.amazonaws.com:9098", icon: OrderToCash, iconColor: "text-pink-400" },
   { name: "Agentic Recruiting & Hiring", href: "http://ec2-44-223-62-175.compute-1.amazonaws.com:8083/", icon: AgenticRecruitingHiringIcon, iconColor: "text-blue-500" },
-  { name: "Legacy Systems Takeover", href: "", icon: LegacySystemsTakeoverIcon, iconColor: "text-pink-400" },
+  { name: "Legacy Systems Takeover", href: "http://lto.wi.roi-roomie.com:8089/", icon: LegacySystemsTakeoverIcon, iconColor: "text-pink-400" },
   { name: "Real-Time Insights", href: "http://ec2-44-223-62-175.compute-1.amazonaws.com:9092/", icon: RealTimeInsightsIcon, iconColor: "text-green-400" },
 ];
 
